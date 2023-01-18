fdc5ce5ea80c833af3d312c1dbded17a0e54c34f
zazac <ienewton123@gmail.com>
2023-01-18 17:29:03 -0300

    Update stellartube.py
    Done testing. It's working

--- stellartube.py
+++ stellartube.py
@@ -3,27 +3,35 @@ from os import listdir as lsdir
 from os.path import exists
 from os import remove
 
-from pytube import YouTube, Playlist
 from yt_dlp.YoutubeDL import YoutubeDL
 
-def configure():
+def configure(format='mp3', quiet=True, verbose=False, folder_path='./', video_code=False, track_numbers=False):
     global options
 
     options = {
-        "quiet": True,
+        "quiet": quiet,
+        "verbose": verbose,
         "format": "bestaudio/best",
         "force_title": True,
         "external_downloader": "native",
         "postprocessors": [{
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'flac',
-            'preferredquality': '192',
-        }],
-
+            'preferredcodec': format,
+            'preferredquality': '192'
+        }]
     }
 
-configure()
-ps = Playlist("https://youtube.com/playlist?list=PLPUc0Mgs4Cs4D8BGffxUag083ek_mhZw2")
+    if video_code:
+        options["outtmpl"] = "%(title)s [%(code)s] .%(ext)s"
+    else:
+        options["outtmpl"] = "%(title)s.%(ext)s"
 
-a = YoutubeDL(options)
-a.download(ps)
+    if track_numbers:
+        options["outtmpl"] = "%(playlist_index)d - " + options["outtmpl"]
+
+    options["outtmpl"] = folder_path + options["outtmpl"]
+
+configure('flac', False, False, "/home/lazuli/Music/Musiquitas/Gris Soundtrack/", False, True)
+
+downloader = YoutubeDL(options)
+downloader.download("https://youtube.com/playlist?list=PLPUc0Mgs4Cs4D8BGffxUag083ek_mhZw2")
