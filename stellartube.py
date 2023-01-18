caeeaba1e693269317a4a23bb7f29c732c1691d3
zazac <ienewton123@gmail.com>
2023-01-18 15:05:36 -0300

    testing with yt_dlp
    With this options, the download turned out to be faster, which was my intention in using another library

--- stellartube.py
+++ stellartube.py
@@ -3,3 +3,27 @@ from os import listdir as lsdir
 from os.path import exists
 from os import remove
 
+from pytube import YouTube, Playlist
+from yt_dlp.YoutubeDL import YoutubeDL
+
+def configure():
+    global options
+
+    options = {
+        "quiet": True,
+        "format": "bestaudio/best",
+        "force_title": True,
+        "external_downloader": "native",
+        "postprocessors": [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'flac',
+            'preferredquality': '192',
+        }],
+
+    }
+
+configure()
+ps = Playlist("https://youtube.com/playlist?list=PLPUc0Mgs4Cs4D8BGffxUag083ek_mhZw2")
+
+a = YoutubeDL(options)
+a.download(ps)
